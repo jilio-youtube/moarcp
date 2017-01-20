691c9ed7408f4680e4a3b6d42a46ba6cae85ab23
jilio-youtube <jiliochannel@gmail.com>
2017-01-20 21:22:35 +0700

    video preview

--- src/App.js
+++ src/App.js
@@ -1,22 +1,47 @@
-import React from 'react'
-import 'bulma'
-import fs from 'fs'
-import ytdl from 'ytdl-core'
+import React, { Component } from "react";
+import "bulma";
+import fs from "fs";
+import ytdl from "ytdl-core";
+import _ from "lodash";
 
-ytdl.getInfo(
-  'http://www.youtube.com/watch?v=A02s8omM_hI',
-  (err, info) => {
-    console.log(info)
-  })
+class App extends Component {
+  constructor() {
+    super();
+    this.state = { url: "", preview: "", busy: false };
+  }
 
-const App = () =>
-  <div className="container" style={{margin: 10}}>
-    <p className="control has-addons">
-      <input className="input" type="text" placeholder="YouTube URL" />
-      <a className="button">
-        🐻 ✊ 💃
-      </a>
-    </p>
-  </div>
+  render() {
+    const button = this.state.busy ? "button is-loading" : "button";
+    return (
+      <div className="container" style={{ margin: 10 }}>
+        <p className="control has-addons">
+          <input
+            className="input"
+            type="text"
+            placeholder="YouTube URL"
+            onChange={this.updateUrl.bind(this)}
+          />
+          <a className={button} onClick={this.downloadCP.bind(this)}>
+            🐻 ✊ 💃
+          </a>
+        </p>
+        <figure class="image is-16by9">
+          <img src={this.state.preview} />
+        </figure>
+      </div>
+    );
+  }
 
-export default App
+  updateUrl(event) {
+    this.setState({ url: event.target.value });
+  }
+
+  downloadCP() {
+    this.setState({ busy: true });
+    ytdl.getInfo(this.state.url, (err, info) => {
+      this.setState({ preview: _.get(info, "iurlhq", ""), busy: false });
+    });
+  }
+}
+
+export default App;
